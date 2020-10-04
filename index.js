49bef651ab5bc89450738f0cf9fe88507f5404d7
Your Name <you@example.com>
2020-10-04 22:26:17 +0100

    :sparkles: send instead of json

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.json({
-        message: "Here's the database :rainbow:"
-    })
+    res.send('This is the DB!')
 })
